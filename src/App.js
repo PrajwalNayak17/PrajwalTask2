bfcb53bac1c768668c91b1e54100c1980a83a78b
PrajwalNayak17 <prajwal.nayak2020@vitstudent.sc.in>
2023-06-21 17:40:42 +0530

    prajwal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
-
+import Task1 from './Components/Task1';
+import Task3 from './Components/Task3';
+import Home from './Components/Home';
+import Task4 from './Components/Task4';
+import Task5 from './Components/Task5';
+import Task2 from './Components/Task2';
 function App() {
+  const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet',
+    'cyan',
+    'magenta',
+    'pink',
+    'purple',
+    'brown',
+    'gray',
+    'black',
+    'white',
+    'silver',
+    'gold',
+    'teal',
+    'lime',
+    'maroon'
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home></Home>} ></Route>
+        <Route exact path='/task1' element={< Task1 />}></Route>
+        <Route exact path='/task2' element={< Task2 />}></Route>
+        <Route exact path='/task3' element={< Task3 colors={colors}/>}></Route>
+        <Route exact path='/task4' element={< Task4 />}></Route>
+        <Route exact path='/task5' element={< Task5 />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
